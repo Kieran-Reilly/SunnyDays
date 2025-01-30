7b058d47653c1ebcc82d7a0cf8bd99240cb37284
Kieran Reilly <kieranpreilly@gmail.com>
2025-01-30 16:06:55 +0200

    Started on Weather Card component

--- src/app.tsx
+++ src/app.tsx
@@ -1,9 +1,9 @@
 import { useState } from "react";
 
+import WeatherCard from "./components/weather_card";
+
 export default function app() {
     return (
-    <div>
-        Hello World
-    </div>
+        <WeatherCard date={new Date()} location="Krung Thep Maha Nakhon" feelsLike="30°C" description="light rain" tempreture="28°C"/>
     );
 }
